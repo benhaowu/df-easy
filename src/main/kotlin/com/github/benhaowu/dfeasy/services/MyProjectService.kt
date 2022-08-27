81b8b11a0bebbc3aaa4913862e6712f26b2c3953
GitHub Action <action@github.com>
2022-08-27 15:30:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/benhaowu/dfeasy/services/MyProjectService.kt
+++ src/main/kotlin/com/github/benhaowu/dfeasy/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.benhaowu.dfeasy.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.benhaowu.dfeasy.MyBundle
 
 class MyProjectService(project: Project) {
 
